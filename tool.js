30df1590a997b97e5432ed1463b6e203f0947257
shamailz <42952907+shamailz@users.noreply.github.com>
2019-10-02 12:20:39 -0400

    Add files via upload

---
+++ tool.js
@@ -0,0 +1,317 @@
+$(document).ready(function(){
+console.log("working or NAH?")
+
+
+$("#name1").click(function () {
+	console.log("heyo")
+	$("div").remove("#name1", "#name2")
+	$("#yellow1").css("opacity" , "1")
+});
+$("#yellow1").click(function () {
+	console.log("heyo")
+	$("div").remove("#name1", "#name2")
+	$("div").remove("#yellow1")
+	$("#yellow2").css("opacity" , "1")
+	$("#yellow2").css("position" , "absolute")
+	$("#yellow2").css("left" , "8vw")
+	$("#yellow2").css("top" , "11vh")
+
+});
+$("#yellow2").click(function () {
+	console.log("heyo")
+	$("#yellow3").css("opacity" , "1")
+	$("#yellow2").css("opacity" , "0")
+	$("#yellow3").css("position" , "absolute")
+	$("#yellow3").css("left" , "8vw")
+	$("#yellow3").css("top" , "11vh")
+});
+$("#yellow3").click(function () {
+	console.log("heyo")
+	$("#yellow3").css("opacity" , "0")
+	$("#yellow4").css("opacity" , "1")
+	$("#yellow4").css("position" , "absolute")
+	$("#yellow4").css("left" , "8vw")
+	$("#yellow4").css("top" , "11vh")
+
+});
+$("#yellow4").click(function () {
+	console.log("heyo")
+	$("#yellow4").css("opacity" , "0")
+	$("#yellow5").css("opacity" , "1")
+	$("#yellow5").css("position" , "absolute")
+	$("#yellow5").css("left" , "8vw")
+	$("#yellow5").css("top" , "11vh")
+
+});
+$("#yellow5").click(function () {
+	console.log("heyo")
+	$("#yellow5").css("opacity" , "0")
+	$("#yellow6").css("opacity" , "1")
+	$("#yellow6").css("position" , "absolute")
+	$("#yellow6").css("left" , "8vw")
+	$("#yellow6").css("top" , "11vh")
+
+});
+$("#yellow6").click(function () {
+	console.log("heyo")
+	$("#yellow7").css("opacity" , "1")
+	$("#yellow7").css("position" , "absolute")
+	$("#yellow7").css("left" , "8vw")
+	$("#yellow7").css("top" , "11vh")
+
+});
+$("#yellow7").click(function () {
+	console.log("heyo")
+	$("#yellow8").css("opacity" , "1")
+	$("#yellow8").css("position" , "absolute")
+	$("#yellow8").css("left" , "8vw")
+	$("#yellow8").css("top" , "11vh")
+
+});
+$("#yellow8").click(function () {
+	console.log("heyo")
+	$("#yellow9").css("opacity" , "1")
+	$("#yellow9").css("position" , "absolute")
+	$("#yellow9").css("left" , "8vw")
+	$("#yellow9").css("top" , "11vh")
+
+});
+$("#yellow9").click(function () {
+	console.log("heyo")
+	$("#yellow10").css("opacity" , "1")
+	$("#yellow10").css("position" , "absolute")
+	$("#yellow10").css("left" , "8vw")
+	$("#yellow10").css("top" , "11vh")
+
+});
+$("#yellow10").click(function () {
+	console.log("heyo")
+	$("#yellow11").css("opacity" , "1")
+	$("#yellow11").css("position" , "absolute")
+	$("#yellow11").css("left" , "8vw")
+	$("#yellow11").css("top" , "11vh")
+
+});
+$("#yellow11").click(function () {
+	console.log("heyo")
+	$("#yellow13").css("opacity" , "1")
+	$("#yellow13").css("position" , "absolute")
+	$("#yellow13").css("left" , "8vw")
+	$("#yellow13").css("top" , "11vh")
+
+});
+$("#yellow13").click(function () {
+	console.log("heyo")
+	$("#yellow14").css("opacity" , "1")
+	$("#yellow14").css("position" , "absolute")
+	$("#yellow14").css("left" , "8vw")
+	$("#yellow14").css("top" , "11vh")
+
+});
+$("#yellow14").click(function () {
+	console.log("heyo")
+	$("#yellow15").css("opacity" , "1")
+	$("#yellow15").css("position" , "absolute")
+	$("#yellow15").css("left" , "8vw")
+	$("#yellow15").css("top" , "11vh")
+
+});
+$("#yellow15").click(function () {
+	console.log("heyo")
+	$("#yellow16").css("opacity" , "1")
+	$("#yellow16").css("position" , "absolute")
+	$("#yellow16").css("left" , "8vw")
+	$("#yellow16").css("top" , "11vh")
+
+});
+$("#yellow16").click(function () {
+	console.log("heyo")
+	$("#yellow17").css("opacity" , "1")
+	$("#yellow17").css("position" , "absolute")
+	$("#yellow17").css("left" , "8vw")
+	$("#yellow17").css("top" , "11vh")
+
+});
+$("#yellow17").click(function () {
+	console.log("heyo")
+	$("#yellow18").css("opacity" , "1")
+	$("#yellow18").css("position" , "absolute")
+	$("#yellow18").css("left" , "8vw")
+	$("#yellow18").css("top" , "11vh")
+
+});
+$("#yellow18").click(function () {
+	console.log("heyo")
+	$("#yellow19").css("opacity" , "1")
+	$("#yellow19").css("position" , "absolute")
+	$("#yellow19").css("left" , "8vw")
+	$("#yellow19").css("top" , "11vh")
+
+});
+
+
+$("#name2").click(function () {
+	console.log("heyo")
+	$("div").remove( "#name2")
+	$("#me1").css("opacity" , "1")
+	$("#me1").css("position" , "absolute")
+	$("#me1").css("left" , "8vw")
+	$("#me1").css("top" , "11vh")
+
+});
+$("#me1").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me1")
+	$("#me2").css("opacity" , "1")
+	$("#me2").css("position" , "absolute")
+	$("#me2").css("left" , "8vw")
+	$("#me2").css("top" , "11vh")
+});
+$("#me2").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me2")
+	$("#me3").css("opacity" , "1")
+	$("#me3").css("position" , "absolute")
+	$("#me3").css("left" , "8vw")
+	$("#me3").css("top" , "11vh")
+});
+$("#me3").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me3")
+	$("#me4").css("opacity" , "1")
+	$("#me4").css("position" , "absolute")
+	$("#me4").css("left" , "8vw")
+	$("#me4").css("top" , "11vh")
+});
+$("#me4").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me4")
+	$("#me5").css("opacity" , "1")
+	$("#me5").css("position" , "absolute")
+	$("#me5").css("left" , "8vw")
+	$("#me5").css("top" , "11vh")
+});
+$("#me5").click(function () {
+	console.log("heyo")
+	$("div").remove( "#name2")
+	$("div").remove("#me5")
+	$("#me6").css("opacity" , "1")
+	$("#me6").css("position" , "absolute")
+	$("#me6").css("left" , "8vw")
+	$("#me6").css("top" , "11vh")
+});
+$("#me6").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me6")
+	$("#me7").css("opacity" , "1")
+	$("#me7").css("position" , "absolute")
+	$("#me7").css("left" , "8vw")
+	$("#me7").css("top" , "11vh")
+});
+$("#me7").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me7")
+	$("#me8").css("opacity" , "1")
+	$("#me8").css("position" , "absolute")
+	$("#me8").css("left" , "8vw")
+	$("#me8").css("top" , "11vh")
+});
+$("#me8").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me8")
+	$("#me9").css("opacity" , "1")
+	$("#me9").css("position" , "absolute")
+	$("#me9").css("left" , "8vw")
+	$("#me9").css("top" , "11vh")
+});
+$("#me9").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me9")
+	$("#me10").css("opacity" , "1")
+	$("#me10").css("position" , "absolute")
+	$("#me10").css("left" , "8vw")
+	$("#me10").css("top" , "11vh")
+});
+$("#me10").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me10")
+	$("#me11").css("opacity" , "1")
+	$("#me11").css("position" , "absolute")
+	$("#me11").css("left" , "8vw")
+	$("#me11").css("top" , "11vh")
+});
+$("#me11").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me11")
+	$("#me12").css("opacity" , "1")
+	$("#me12").css("position" , "absolute")
+	$("#me12").css("left" , "8vw")
+	$("#me12").css("top" , "11vh")
+});
+$("#me12").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me12")
+	$("#me13").css("opacity" , "1")
+	$("#me13").css("position" , "absolute")
+	$("#me13").css("left" , "8vw")
+	$("#me13").css("top" , "11vh")
+});
+$("#me13").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me13")
+	$("#me14").css("opacity" , "1")
+	$("#me14").css("position" , "absolute")
+	$("#me14").css("left" , "8vw")
+	$("#me14").css("top" , "11vh")
+});
+$("#me14").click(function () {
+	console.log("heyo")
+	$("div").remove("#name1", "#name2")
+	$("div").remove("#me14")
+	$("#me15").css("opacity" , "1")
+	$("#me15").css("position" , "absolute")
+	$("#me15").css("left" , "8vw")
+	$("#me15").css("top" , "11vh")
+});
+$("#me15").click(function () {
+	console.log("heyo")
+$("div").remove( "#name2")
+	$("div").remove("#me15")
+	$("#me16").css("opacity" , "1")
+	$("#me16").css("position" , "absolute")
+	$("#me16").css("left" , "8vw")
+	$("#me16").css("top" , "11vh")
+});
+$("#me16").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me16")
+	$("#me17").css("opacity" , "1")
+	$("#me17").css("position" , "absolute")
+	$("#me17").css("left" , "8vw")
+	$("#me17").css("top" , "11vh")
+});
+$("#me17").click(function () {
+	console.log("heyo")
+	$("div").remove("#name2")
+	$("div").remove("#me17")
+	$("#me18").css("opacity" , "1")
+	$("#me18").css("position" , "absolute")
+	$("#me18").css("left" , "8vw")
+	$("#me18").css("top" , "11vh")
+});
+
+
+});
